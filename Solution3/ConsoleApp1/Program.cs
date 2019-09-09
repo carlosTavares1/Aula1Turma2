4966bf3749a2844b3b14b6b19fcbdafc420e1ee4
Sss41 <silvana.souza63@gmail.com>
2019-09-09 15:58:17 -0300

    Exemplo de repetição na primeira aula

---
+++ Solution3/ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			for(int i = 1; i <= 1000000; i = i + 5)
+			{
+				Console.WriteLine(i);
+			}
+			Cliente c = new Cliente();
+		}
+	}
+}
